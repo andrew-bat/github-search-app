9384514470c3ef9ad40c017b7fdf59034bd9c5e0
andrew-bat <mister.dron1501@gmail.com>
2020-10-04 13:21:49 +0300

    created main-layout-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {MainLayoutComponent} from './shared/layouts/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
